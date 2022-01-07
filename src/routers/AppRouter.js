18bfb18b94f4ade2146eef5a6543196c2fb41d3a
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-07 18:59:47 -0400

    Spinner when loading before render AppRouter

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,15 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
+import { Spinner } from '../components/iu/Spinner';
 import { AuthRouter } from './AuthRouter';
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { useDispatch } from 'react-redux';
 import { login } from '../actions/auth';
 
-
 export const AppRouter = () => {
-  
+
+  const [checking, setChecking] = useState(true);
+  // to check if someone is authenticated before using routes
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // if logged cannot see login screen | private and public routes
+
   const dispatch = useDispatch()
 
   // this is very important to keep auth when reloading
@@ -20,11 +25,19 @@ export const AppRouter = () => {
       console.log(user)
       if (user?.uid) {
         // means that user has properties and this wont crash
-        dispatch(login(user.uid, user.displayName))
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true)
+      } else {
+        setIsLoggedIn(false)
       }
     })
+    setChecking(false);
     // dependency of dispatch does not change actually
-  }, [dispatch])
+  }, [ dispatch, checking, isLoggedIn ])
+
+  if (checking) {
+    return (<Spinner />)
+  }
 
   return (
     <BrowserRouter>
