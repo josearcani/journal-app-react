84574d1eb562a01996e27b319c66e8f1eed9f3c7
joseArc <juanjose.arcani2309@gmail.com>
2022-01-11 14:35:19 -0400

    feat: show new entry
    now after creating a new document with unique id, we dispatch id and newNote object to noteActive action so that notesReducer fill up the active property with the newNote and appear in the app

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,5 +1,6 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../firebase/firebase-config";
+import { types } from "../types/types";
 
 export const startNewNote = () => {
   return async (dispatch, getState) => {
@@ -11,9 +12,16 @@ export const startNewNote = () => {
       body: '',
       date: new Date().getTime(),
     }
-    
+    // https://firebase.google.com/docs/firestore/manage-data/add-data#add_a_document
     const docRef = await addDoc(collection(db ,`${uid}/journal/notes`), newNote);
-
-    console.log(docRef);
+    dispatch(noteActive(docRef.id, newNote))
   }
 }
+
+export const noteActive = (id, newNote) => ({
+  type: types.notesActive,
+  payload: {
+    id,
+    ...newNote
+  }
+})
--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -14,16 +14,21 @@
 
 */
 
+import { types } from "../types/types";
+
 const initialState = {
   notes: [],
   active: null
 }
 export const notesReducer = (state = initialState, action) => {
   switch (action.type) {
-    case action:
-      
-      break;
-  
+    case types.notesActive:
+      return {
+        ...state,
+        active: {
+          ...action.payload,
+        }
+      }
     default:
       return state;
   }
