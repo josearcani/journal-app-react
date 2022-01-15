01e73489eb10e184a2ee7b11b58f4f283d69295f
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-14 21:40:24 -0400

    fix: fileUpload test require setimmediate pkg
    setimmediate pkg was necessary to use cloudinary sdk to delete the uploaded test img

--- src/__tests__/helpers/fileUpload.test.js
+++ src/__tests__/helpers/fileUpload.test.js
@@ -1,4 +1,13 @@
+import 'setimmediate';
 import { fileUpload } from '../../helpers/fileUpload';
+import cloudinary from 'cloudinary';
+
+cloudinary.config({ 
+  cloud_name: 'jarh-workplace', 
+  api_key: '549136979557743', 
+  api_secret: 'H1GrMxKiabMNmyqZPQjFuR93Fig',
+  secure: true
+});
 
 describe('Test fileUpload', () => {
   
@@ -10,6 +19,11 @@ describe('Test fileUpload', () => {
     const url = await fileUpload(file);
 
     expect(typeof url).toBe('string');
+
+    // get publiv_id ===> fileName without extension
+    const segments = url.split('/');
+    const imageId = segments[ segments.length - 1 ].replace('.png','');
+    cloudinary.v2.api.delete_resources(imageId, () => {});
   })
   
   test('should receive null', async () => {
