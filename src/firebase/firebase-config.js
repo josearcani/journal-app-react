f84bacff2ad2888e228f36f1d23f9fee4ea1cd20
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-15 11:58:37 -0400

    fix: set .env.test.local and .env.development.local
    now the app can switch witch firebase instance to use in dev and test mode following the .env.example

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -4,32 +4,33 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { GoogleAuthProvider } from 'firebase/auth';
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAv30DCKQLGl7ELxQPLqFOrlnv7f2hcDcU",
-  authDomain: "react-app-udemy-52a38.firebaseapp.com",
-  projectId: "react-app-udemy-52a38",
-  storageBucket: "react-app-udemy-52a38.appspot.com",
-  messagingSenderId: "102652422381",
-  appId: "1:102652422381:web:17a11232a76a38dfa8c4e4"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
-const firebaseConfigTesting = {
-  apiKey: "AIzaSyCMyU5ArcC33s4ylvoY7m9h8NAW_1emHlw",
-  authDomain: "firestore-test-journal.firebaseapp.com",
-  projectId: "firestore-test-journal",
-  storageBucket: "firestore-test-journal.appspot.com",
-  messagingSenderId: "419151579130",
-  appId: "1:419151579130:web:dbe842f2cec2926078c643"
-};
+// const firebaseConfigTesting = {
+//   apiKey: "AIzaSyCMyU5ArcC33s4ylvoY7m9h8NAW_1emHlw",
+//   authDomain: "firestore-test-journal.firebaseapp.com",
+//   projectId: "firestore-test-journal",
+//   storageBucket: "firestore-test-journal.appspot.com",
+//   messagingSenderId: "419151579130",
+//   appId: "1:419151579130:web:dbe842f2cec2926078c643"
+// };
 
-if (process.env.NODE_ENV === 'test') {
-  // test env
-  initializeApp(firebaseConfigTesting);
-} else {
-  // produc or dev
-  initializeApp(firebaseConfig);
-}
+// if (process.env.NODE_ENV === 'test') {
+//   // test env
+//   initializeApp(firebaseConfigTesting);
+// } else {
+//   // produc or dev
+//   initializeApp(firebaseConfig);
+// }
+
+initializeApp(firebaseConfig);
 
 // inicialize firebase
 const db = getFirestore();
