d327475ac7f83b471835528f195c100aeb7c35e2
jose arcani <juanjose.arcani2309@gmail.com>
2022-01-14 20:27:23 -0400

    test: setting test environment and first test

---
+++ src/__tests__/types/types.test.js
@@ -0,0 +1,28 @@
+import { types } from '../../types/types';
+
+describe('Test on types file', () => {
+
+  const  testTypes = {
+    login: '[Auth] Login',
+    logout: '[Auth] Logout',
+  
+    uiSetError: '[UI] Set Error',
+    uiRemoveError: '[UI] Remove Error',
+  
+    uiStartLoading: '[UI] Start loading',
+    uiFinishLoading: '[UI] Finish loading',
+  
+    notesAddNew: '[Notes] New note',
+    notesActive: '[Notes] Set active note',
+    notesLoad: '[Notes] Load notes',
+    notesUpdate: '[Notes] Updated note',
+    notesFileUrl: '[Notes] Updated image url',
+    notesDelete: '[Notes] Delete note',
+    notesLogoutCleaning: '[Notes] Logout cleaning',
+  }
+  
+  test('should be the same', () => {
+    expect(types).toEqual(testTypes);
+  })
+  
+})
